6cff51884788f1a3a4bb554bc6c49f50b9edb67a
João Mangueira <jamjs7@gmail.com>
2023-01-25 11:58:36 -0300

    criando componentes de checkbox e backButton. Criando Screens de Habit e New. E fiz algumas configurações de navegação.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import {View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Header } from '../components/Header';
 import { DAY_SIZE, HabitDay } from '../components/HabitDay';
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
