44be7308468285548d6cbda84442c8ff47e99edb
João Mangueira <jamjs7@gmail.com>
2023-01-26 17:33:19 -0300

    checkbox animação

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,9 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import Animated,
+{
+  RotateInUpLeft
+} from 'react-native-reanimated';
 import  colors from 'tailwindcss/colors';
 
 interface Props extends TouchableOpacityProps {
@@ -15,13 +19,16 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
     >
       {
         checked ?
-          <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+          <Animated.View
+            className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+            entering={RotateInUpLeft}
+          >
             <Feather
               name="check"
               size={20}
               color={colors.white}
             />
-          </View>
+          </Animated.View>
           :
           <View className="h-8 w-8 bg-zinc-900 rounded-lg " />
       }
